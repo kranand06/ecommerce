cca4fd8e64bf056d54361ce9557446127b3153f8
Kumar Anand <kranand9430@gmail.com>
2025-03-08 14:00:15 +0530

    tailwind and router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <App />,
+      },
+      {
+        path: '*',
+        element: <App />,
+      },
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
