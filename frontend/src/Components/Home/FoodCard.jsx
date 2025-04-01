2d2f7dd795a758122db21d02cd769c16ea1ebc6d
Kumar Anand <kranand9430@gmail.com>
2025-04-01 20:03:48 +0530

    categories functional

--- frontend/src/Components/Home/FoodCard.jsx
+++ frontend/src/Components/Home/FoodCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import Food  from "./menu.js"
+import Food from "./menu.js"
 
-function FoodCard() {
+function FoodCard({ category }) {
   return (
     <>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-                {Food.map((item) => (
-                    <div
-                        key={item.id}
-                        // onClick={() => handleInput("budget", item.title)}
-                        className={`p-3 border rounded-lg cursor-pointer hover:shadow-lg transition-all duration-200`}
-                    >
-                        <img className='w-full rounded-lg' src={item.image} alt="logo" />
-                        <h2 className="font-bold text-xl mt-3">{item.title}</h2>
-                        <h2 className="text-sm text-gray-500 mt-2">{item.desc}</h2>
-                    </div>
-                ))}
-            </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 " >
+        {Food.map((item) => (
+          <div
+            key={item.id}
+            className={`p-3 border rounded-lg cursor-pointer hover:shadow-lg transition-all duration-200 ${
+              category === "View All" ? "" : (item.cat === category ? "" : "hidden")
+            }`}          >
+            <img className='w-full rounded-lg' src={item.image} alt="logo" />
+            <h2 className="font-bold text-xl mt-3">{item.title}</h2>
+            <h2 className="text-sm text-gray-500 mt-2">{item.desc}</h2>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
--- frontend/src/Components/Home/Menu.jsx
+++ frontend/src/Components/Home/Menu.jsx
@@ -5,19 +5,19 @@ import FoodCard from './FoodCard.jsx'
 
 function Menu() {
 
-    const [category, setCategory] = useState("")
+    const [category, setCategory] = useState("View All")
 
 
     return (
         <>
-            <h1 className='mt-5 text-2xl font-medium'>Choose from categories...</h1>
+            <h1 id='menu' className='mt-5 text-2xl font-medium'>Choose from categories...</h1>
             <div className="flex  gap-8 mt-5 items-center pb-5 overflow-x-auto overflow-y-hidden whitespace-nowrap max-w-full">
                 {Category.map((item) => (
                     <div
                         key={item.id}
                         onClick={() => {
                             setCategory(item.title)
-                            console.log(item.title);}}
+                        }}
                         className="w-48 p-3 cursor-pointer hover:scale-110 transition-all duration-200 flex-shrink-0"
                     >
                         <img className="rounded-full aspect-square object-cover" src={item.image} alt="logo" />
@@ -29,7 +29,7 @@ function Menu() {
 
             <hr className='my-5' />
             <h1 className='mt-10 text-3xl font-semibold'>Top Dishes near you.</h1>
-            <FoodCard />
+            <FoodCard category={category} />
 
         </>
     )
