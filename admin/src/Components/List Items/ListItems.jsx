096e0b810bf5c59ca0aedb22caf52733b3e16d12
Kumar Anand <kranand9430@gmail.com>
2025-04-06 22:48:10 +0530

    list item fetch

--- admin/src/Components/List Items/ListItems.jsx
+++ admin/src/Components/List Items/ListItems.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 
 function ListItems() {
 
-  return(
-    <div>
 
-List Items
+  const url = "http://localhost:3000";
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.status === 200) {
+        setList(response.data);
+        toast.success("List fetched successfully! 🚀", { position: "bottom-right" });
+      }
+    } catch (error) {
+      toast.error("Error fetching list! 🥲", { position: "bottom-right" });
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, [])
+
+
+
+
+  return (
+    <div>
+      {/* <button onClick={fetchList}>fetching</button> */}
+
+      <ToastContainer />
     </div >
   )
 }
